d22e8f1c54389728fbc97a20f27c82f57e485386
irensula <irinawebdev@gmail.com>
2024-03-17 22:58:34 +0200

    Update clusterMap.js

--- public/javascripts/clusterMap.js
+++ public/javascripts/clusterMap.js
@@ -2,7 +2,7 @@
     const map = new mapboxgl.Map({
         container: 'cluster-map',
         // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
-        style: 'mapbox://styles/mapbox/light-v11',
+        style: 'mapbox://styles/mapbox/streets-v12',
         center: [27.008137, 64.800324],
         zoom: 4.2
     });
